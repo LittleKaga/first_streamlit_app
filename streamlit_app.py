85132087251bb79a46915586034430c8cba7de0e
Prasad Patil <45709069+LittleKaga@users.noreply.github.com>
2022-11-18 10:07:39 +0000

    Update streamlit_app.py
    Function Error correction 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruitvice Fruit Advice!')
-def get_fruitvice_data(this_fruit_choice)
+def get_fruitvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     #streamlit.text(fruityvice_response.json())
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
