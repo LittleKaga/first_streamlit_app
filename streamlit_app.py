06093ece2ae1664687891583efa1c2bc3fc05d0d
Prasad Patil <45709069+LittleKaga@users.noreply.github.com>
2022-11-18 13:20:18 +0000

    Update streamlit_app.py
    insert fruit check

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,8 @@ if streamlit.button('Get Fruit Load List'):
 
    
 #streamlit.stop()
-#add_my_fruit = streamlit.text_input('What fruit would you like like to add?')
-#streamlit.write('Thanks for adding', add_my_fruit)
-#my_cur.execute("insert into fruit_load_list values('from streamlit')")
+add_my_fruit = streamlit.text_input('What fruit would you like like to add?')
+streamlit.write('Thanks for adding', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
