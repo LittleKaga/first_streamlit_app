f4370cc05de369890b19959d6770d3ff280211f0
Prasad Patil <45709069+LittleKaga@users.noreply.github.com>
2022-11-18 16:18:05 +0000

    Update streamlit_app.py
    Corrected mislabelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,13 +39,13 @@ except URLError as e:
   streamlit.error()
   
 
-streamlit.header("My food load list contains:")
+streamlit.header("View Our Fruit List - Add your favourite!")
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
       my_cur.execute("SELECT * FROM fruit_load_list")
       return my_cur.fetchall() #fetchone()
 
-if streamlit.button('View Our Fruit List - Add your favourite!'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    my_cnx.close()
