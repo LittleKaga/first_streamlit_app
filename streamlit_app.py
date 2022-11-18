d2acddc38e41f9f6e480ab02ee8151dffa802a72
Prasad Patil <45709069+LittleKaga@users.noreply.github.com>
2022-11-18 10:28:29 +0000

    Update streamlit_app.py
    button if error correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def get_fruit_load_list():
       my_cur.execute("SELECT * FROM fruit_load_list")
       my_data_rows = my_cur.fetchall() #fetchone()
 
-if steamlit.button('Get Fruit Load List')
+if steamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    streamlit.dataframe(my_data_rows)
 
